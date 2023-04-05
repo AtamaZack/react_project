2fe2bd80add6da88b263c2157efda72faecdaa8a
ZackAtama <atama.zack@gmail.com>
2023-04-05 07:57:58 +0300

    Ft(App.js/App.css): Edit the App.js and App.css files
    This commit edits the following files
    - App.js: removes boilerplate code and adds new elements to the file
    - App.css: adds styling to added elements
    - Assets folder: adds an image to the folder to be displayed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import img1 from './assets/img1.jpg'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ul className='navbar'>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Products & Services</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+			<img
+				src={img1}
+				alt="Image 1"
+				className='main-img'
+			/>
+			<p
+				className='paragraph1'
+			>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+				molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+				numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+				optio, eaque rerum! Provident similique accusantium nemo autem.
+				<br/>
+				<br/>
+			<hr/>
+			</p>
+		</div>
+	);
 }
 
 export default App;
